28002c47ec96eef3b69d81485479362ecb559623
Yasas <UYasaDi@PEROOT.COM>
2018-09-27 21:52:40 +0530

    add mehtod to make http post request

--- src/main/java/com/element/analytics/elasticSearch/Dao/FacebookDAO.java
+++ src/main/java/com/element/analytics/elasticSearch/Dao/FacebookDAO.java
@@ -1,6 +1,9 @@
 package com.element.analytics.elasticSearch.Dao;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -8,10 +11,15 @@ import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.elasticsearch.action.admin.indices.get.GetIndexRequest;
 import org.elasticsearch.action.bulk.BulkProcessor;
 import org.elasticsearch.action.bulk.BulkRequest;
@@ -575,6 +583,7 @@ public class FacebookDAO {
 		JSONObject requestObj = new JSONObject()
 				.put("data", comments);
 
+		
 //		RestTemplate restTemplate = new RestTemplate();
 //		
 //		MappingJackson2HttpMessageConverter jsonHttpMessageConverter = new MappingJackson2HttpMessageConverter();
@@ -617,6 +626,45 @@ public class FacebookDAO {
 		
 		return returnObj;
 	}
+	
+	private void sendPost() throws Exception {
+
+		String returnObj = new JSONObject()
+				.put("success", "dsd")
+				.put("message", "dsds")
+				.put("data", "adsdd").toString();
+		
+		String url = "https://selfsolve.apple.com/wcResults.do";
+
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpPost post = new HttpPost(url);
+
+		post.setHeader("Accept", "application/json");
+		post.setHeader("Content-type", "application/json");
+	    
+		 StringEntity entity = new StringEntity(returnObj);
+
+		post.setEntity(entity);
+
+		HttpResponse response = httpclient.execute(post);
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + post.getEntity());
+		System.out.println("Response Code : " + 
+                                    response.getStatusLine().getStatusCode());
+
+		BufferedReader rd = new BufferedReader(
+                        new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+
+		System.out.println(result.toString());
+
+	}
+
 }
 
 //Connection<Post> myFeed = facebookClient.fetchConnection(pageId + "/feed", Post.class);
